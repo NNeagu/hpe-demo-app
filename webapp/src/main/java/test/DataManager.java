a797c35b4d63e3156bd36f536c69949bd55fa9a6
emiller58 <64948864+emiller58@users.noreply.github.com>
2021-05-10 13:18:56 +0300

    user story #1006

--- webapp/src/main/java/test/DataManager.java
+++ webapp/src/main/java/test/DataManager.java
@@ -12,7 +12,7 @@ import java.net.URISyntaxException;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
-//emiller - commit1005
+//emiller - commit1006
 /**
  * Created with IntelliJ     IDEA.
  * User: gullery some change 23
